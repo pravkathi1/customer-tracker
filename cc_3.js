de5770c288136ed9e0f14dd4045bb69ce8f86513
Pravallika <pkathi@usf.edu>
2025-09-11 17:27:31 -0400

    first commit

---
+++ cc_3.js
@@ -0,0 +1,9 @@
+// Coding Challenge 3A
+
+let customers = [
+    {email:"asmitha@gmail.com", name:"Asmitha", purchases: 4},
+    {email:"sree@gmail.com", name:"Sree", purchases: 7},
+    {email:"adhira@gmail.com", name:"Adhira", purchases: 9},
+]
+
+customers.forEach(obj => console.log(`Customer Email: ${obj.email}\t | \t Customer Name: ${obj.name} | \t Purchases: ${obj.purchases}\t`))
