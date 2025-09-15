61b9c89feeff5531ddbfcb44dd12f523481512a2
Pravallika <pkathi@usf.edu>
2025-09-14 23:31:24 -0400

    commit

--- cc_3.js
+++ cc_3.js
@@ -6,22 +6,18 @@ let customers = [
     {email:"adhira@gmail.com", name:"Adhira", purchases: ["Laptop", "Headphones", "Mouse"]},
 ]
 
-customers.forEach(obj => console.log(`Customer Email: ${obj.email}\t | \t Customer Name: ${obj.name} | \t Purchases: ${obj.purchases}\t`))
+customers.forEach(obj => console.log(`Customer Email: ${obj.email}\t | \t Customer Name: ${obj.name} | \t Purchases: ${obj.purchases}\t`));
 
-customers.push({email: "pravallika@gmail.com", name: "Pravallika", purchases: ["Monitor", "Webcam"]})
+customers.push({email: "pravallika@gmail.com", name: "Pravallika", purchases: ["Monitor", "Webcam"]});
 
 console.log(customers);
 
-let removedCustomer = customers.shift();
-console.log("Removed Customer:", removedCustomer);
+console.log("Removed Customer:", customers.shift());
 
 customers.forEach(c => {if (c.name === "Sree") {c.email = "sree123@usf.edu";}});
 
 customers.forEach(c => {if (c.name === "Adhira") {c.purchases.push("Smartwatch");}});
 
 console.log("Updated Customers:");
-customers.forEach(c => {console.log(`Name: ${c.name} | Email: ${c.email} | Purchases: ${c.purchases.join(", ")}`);});
+customers.forEach(c => {console.log(`Name: ${c.name} | Email: ${c.email} | Purchases: ${c.purchases.length}`);});
 
-const purchases = ["Laptop", "Monitor", "Keyboard"];
-let totalPurchases = purchases.length;
-console.log(totalPurchases); 
